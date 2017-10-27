93d857952cb808abedd24cfc0d64711aeeefafa2
Kaci <kaci.childers@gmail.com>
2017-10-27 12:25:39 -0500

    added more levels

--- rickandmorty.asm
+++ rickandmorty.asm
@@ -23,20 +23,60 @@
 	
 	opt4_rickdemption: .asciiz "DAMN YOU SELFISH MORTY!!! AF-AFTER ALL I'VE DONE FOR YOU MORTY!!!"
 
-	opt5_rickdemption: .asciiz "Well hurt up and dig them up! You can use the dead ricks portal gun! \n Come onn Morty! D-Did you seriously not think about that! \n (press enter to dig)"	
+	opt5_rickdemption: .asciiz "Well hurt up and dig them up! You can use the dead ricks portal gun! \n Come onn Morty! D-Did you seriously not think about that! \n (press enter to dig) \n"	
 	
-	opt6_rickdemption: .asciiz "Whoops! I must have added that to the morty mind blowers list \n (7) ask about morty mind blowers \n (8) Hurry to pick up a shovel and dig \n ====="
+	opt6_rickdemption: .asciiz "Whoops! I must have added that to the morty mind blowers list \n (7) ask about morty mind blowers \n (8) Hurry to pick up a shovel\n ====="
 	
-	opt7_rickdemption: .asciiz "We don't have time for these questions! Just help summber dig!"
+	opt7_rickdemption: .asciiz "We don't have time for these questions! Just help summber dig! \n (press enter to pick up a shovel) \n"
 	
-	opt8_rickdemption: .asciiz "You've got the gun! Great! \n "
+	opt8_rickdemption: .asciiz "You've got the gun! Great! \n \n "
 	
-	opt9_rickdemption: .asciiz "HURRY CONROY IS COMMING! G-GET OUT OF THERE MORTY! \n (10) give gun to Conroy so he can 'Relinquish the illegal technology to the nearest Federation representative. ['"
+	opt82_rickdemption: .asciiz "HURRY CONROY IS COMMING! G-GET OUT OF THERE MORTY! \n (9) give gun to Conroy so he can 'Relinquish the illegal technology \n    to the nearest Federation representative.' \n (10) Take Summer to Dimension C-137 \n ====="
 
-	opt10_rickdemption: .asciiz " "
+	opt9_rickdemption: .asciiz "NO MORTY! THERE GOES EVERY CHANCE OF ME EXCAPING SPACE PRISON!! \n G-Got YOu! im a freaking genius !\n I can do this o-on *buurrrp* my own "
 
-	opt11_rickdemption: .asciiz " "
-					
+	opt10_rickdemption: .asciiz "Why would you go here Morty!?!? We left this demention for a reason!! T-This is not space prison! \n Summer:' What is this place?' \n (11) Walk with her down the street until you find your old home \n (12) Tell her this was your home, and your original family is here \n ===== "
+	
+	opt11_rickdemption: .asciiz "Summer: ''Hey that totally looks like our house!'' \n (13)Tell her this is your original home \n (14)follow her inside \n ====="
+
+	opt12_rickdemption: .asciiz "Summer: What? How did this happen? Totally uncool! \n (13) Explain to her that this is the side of the adventures that no one sees\n (14)Walk with her to original home \n ===== "
+
+	opt13_rickdemption: .asciiz "Summer: '' Grandpa Rick would never do this! Hes like a hero! '' "
+	
+	opt14_rickdemption: .asciiz "You see your original family \n (15)\n (16) "	
+	
+	opt15_rickdemption: .asciiz " "	
+	
+	opt16_rickdemption: .asciiz " "	
+	
+	opt17_rickdemption: .asciiz " "	
+	
+	opt18_rickdemption: .asciiz " "	
+	
+	opt19_rickdemption: .asciiz " "
+	
+	opt20_rickdemption: .asciiz " "		
+
+	opt21_rickdemption: .asciiz " "	
+	
+	opt22_rickdemption: .asciiz " "	
+	
+	opt23_rickdemption: .asciiz " "	
+	
+	opt24_rickdemption: .asciiz " "	
+	
+	opt25_rickdemption: .asciiz " "	
+	
+	opt26_rickdemption: .asciiz " "
+	
+	opt27_rickdemption: .asciiz " "	
+
+	opt28_rickdemption: .asciiz " "	
+	
+	opt29_rickdemption: .asciiz " "
+	
+	opt30_rickdemption: .asciiz " "	
+		
 .text
 	la $v0, 4
 	la $a0, intro
@@ -139,7 +179,7 @@
 	li $v0, 12
 	syscall
 	
-	b opt8
+	b opt82
 	
 	opt6:
 	li $v0, 4
@@ -165,37 +205,104 @@
 	li $v0, 12
 	syscall
 	
-	b level2
+	b opt5
 	
 	opt8:
+	b opt5
+	
+	opt82:
 	li $v0, 4
 	la $a0, opt8_rickdemption
 	syscall
 	
-	la $t0, ($v0)
+	li $v0, 4
+	la $a0, opt82_rickdemption
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	la $t0, ($v0)                                   
 	beq $t0, 0, rules
 	beq $t0, 9, opt9
 	beq $t0, 10, opt10
 	syscall
-	
-	b wrong
+
+	b level2
 
 	opt9:
 	li $v0, 4
 	la $a0, opt9_rickdemption
 	syscall
 	
-	la $t0, ($v0)
+	b level2	
+	
+	opt10:
+	li $v0, 4
+	la $a0, opt10_rickdemption
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	la $t0, ($v0)                                   
 	beq $t0, 0, rules
 	beq $t0, 11, opt11
 	beq $t0, 12, opt12
 	syscall
-	opt10:
-	
-	opt11:
-	
-	opt12:
 
+	b level2
+		
+	opt11:
+	li $v0, 4
+	la $a0, opt11_rickdemption
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	la $t0, ($v0)                                   
+	beq $t0, 0, rules
+	beq $t0, 13, opt13
+	beq $t0, 14, opt14
+	syscall
+
+	b level2
+			
+	opt12:
+	li $v0, 4
+	la $a0, opt12_rickdemption
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	la $t0, ($v0)                                   
+	beq $t0, 0, rules
+	beq $t0, 13, opt13
+	beq $t0, 14, opt14
+	syscall
+
+	b level2
+		
+	opt13:
+	li $v0, 4
+	la $a0, opt13_rickdemption
+	syscall
+	
+	li $v0, 5
+	syscall
+	
+	la $t0, ($v0)                                   
+	beq $t0, 0, rules
+	beq $t0, 13, opt13
+	beq $t0, 14, opt14
+	syscall
+
+	b level2
+	
+	opt14:
+	
 	level2:
 	
 	wrong:
